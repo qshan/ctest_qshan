9f35cc40c060705d2075c6dd6ed0ba2ccca10281
Frank Shan <frankqs0304@gmail.com>
2021-08-14 17:55:20 +0800

    update the print format for test06

--- 06_test_gcc/src/test06.c
+++ 06_test_gcc/src/test06.c
@@ -2,8 +2,20 @@
 #include "say_hello_06.h"
 int main()
 {
+  printf("\n");
+  printf("===================================================\n");
   printf("hello test06 \n");
+  printf("\n");
   say_hello06();
+
+
+  //the contents of main function here
+  printf("The function contents stat run \n");
+
+
+  printf("\n");
+  printf("Bye test06 \n");
+  printf("===================================================\n");
   return 0;
 
 }
