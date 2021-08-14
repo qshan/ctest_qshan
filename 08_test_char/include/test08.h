3874dfe5721003e1db40375eb36562ce06df3180
Frank Shan <frankqs0304@gmail.com>
2021-08-14 16:43:11 +0800

    update the string operation, and add func folder

---
+++ 08_test_char/include/test08.h
@@ -0,0 +1,35 @@
+#ifndef __TEST08_H__
+#define __TEST08_H__
+  char *ustring_array_test08[]= /*pointer array, the element is point to constant string*/
+  {
+      "00 This is contents of element of string array test08"
+    , "01  This is contents of element of string array test08"
+    , "02   This is contents of element of string array test08"
+    , "03    This is contents of element of string array test08"
+    , "04     This is contents of element of string array test08"
+    , "05      This is contents of element of string array test08"
+    , "06       This is contents of element of string array test08"
+    , "07        This is contents of element of string array test08"
+    , "08         This is contents of element of string array test08"
+    , "09          This is contents of element of string array test08"
+    , "10           This is contents of element of string array test08"
+    , "11            This is contents of element of string array test08"
+  };
+
+  int uarray_test08[]=
+  {
+      0x0
+    , 0x01
+    , 0x02
+    , 0x03
+    , 0x04
+    , 0x05
+    , 0x06
+    , 0x07
+    , 0x08
+    , 0x09
+    , 0x10
+    , 0x11
+  };
+
+#endif
