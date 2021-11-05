95965b704393837a6f115db8c11fda9e7a3c8fd5
Frank Shan <frankqs0304@hotmail.com>
2021-11-06 03:10:20 +0800

    add argument search code

--- 14_test_read_command_args/test/header/test14.h
+++ 14_test_read_command_args/test/header/test14.h
@@ -1,9 +1,11 @@
 #ifndef __TEST14_H__
 #define __TEST14_H__
 
-#define PRINT_DEBUG_ENABLE 0
+#define PRINT_DEBUG_ENABLE 1
 
 #include "test14_shared.h"
 #include "say_hello_14.h"
+
+#include "string.h"
 
 #endif
--- 14_test_read_command_args/test/src/test14.c
+++ 14_test_read_command_args/test/src/test14.c
@@ -4,7 +4,7 @@
 //#include "test14_shared.h"
 int main(int argc, char *argv[])
 {
-  int i;//for arg print control
+  int i, j;//for arg print control
   int value_returned=0;
 
   #if PRINT_DEBUG_ENABLE
@@ -24,6 +24,7 @@ int main(int argc, char *argv[])
         );
   #endif
 
+  #if 1
   {
     printf("------------------------------\n");
     //printf("#####Get argument is %s\n", *argv);
@@ -35,12 +36,72 @@ int main(int argc, char *argv[])
     }
     printf("\n");
 
+    printf("------------------------------\n");
+
     for(i=0;i<argc;i++)
     {
       printf("#####Get argv[%d] is %s\n" ,i ,argv[i]);
     }
-    printf("------------------------------\n");
   }
+  #endif
+
+  #if 0
+  {
+    printf("------------------------------\n");
+
+    for(i=1;i<argc;)
+    {
+      if ((strcmp (argv[i], "--port") == 0))
+      {
+        printf("#####get --port info: %s\n", argv[i+1]);
+      }
+
+      if ((strcasecmp (argv[i], "--file") == 0))
+      {
+        printf("#####get --file info: %s\n", argv[i+1]);
+      }
+
+      i=i+2;
+    }
+
+  }
+  #endif
+
+  #if 1
+  {
+    #define MAX_STRING_LENGTH 100
+    //char input_arg_list[30][100]=
+    char input_arg_list[][2][MAX_STRING_LENGTH]=
+    {
+       {"--port", "need_input"}
+      ,{"--file", "need_input"}
+      ,{"--end--", "--end--"}
+    };
+    printf("------------------------------\n");
+
+    for(i=1;i<argc;)
+    {
+      //printf("##### Check argv[%d] info: %s\n" ,i ,argv[i]);
+      for (j=0;;j++)
+      {
+        //printf("##### Check input_arg_list[%d][0] info: %s\n" ,i ,input_arg_list[j][0]);
+        if ((strcasecmp (input_arg_list[j][0], "--end--") == 0)){break;}
+
+        //if (!strcmp (argv[i], "--file"))
+        if ((strcasecmp (argv[i], input_arg_list[j][0]) == 0))
+        {
+          //printf("#####get input_arg_list[j][0] info: %s\n", argv[i+1]);
+          //strcpy(input_arg_list[j][1], argv[i+1]);
+          //strncpy(input_arg_list[j][1], argv[i+1] ,100);
+          strncpy(input_arg_list[j][1], argv[i+1] ,strlen(argv[i+1]));
+          printf("#####Get %s info: %s\n" ,input_arg_list[j][0] ,input_arg_list[j][1]);
+        }
+      }
+      i=i+2;
+    }
+
+  }
+  #endif
 
   #if PRINT_DEBUG_ENABLE
     printf("\n");
