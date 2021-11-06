fe8ec4a71c66a94547024e427b7f6bc8ea217b79
Frank Shan <frankqs0304@hotmail.com>
2021-11-06 14:02:19 +0800

    back version, worked, before add arg_read module

--- 14_test_read_command_args/test/src/test14.c
+++ 14_test_read_command_args/test/src/test14.c
@@ -27,9 +27,9 @@ int main(int argc, char *argv[])
   #if 1
   {
     printf("------------------------------\n");
-    //printf("#####Get argument is %s\n", *argv);
-    printf("#####Get argc is %d\n"    , argc);
-    printf("#####Get argv is ");
+    //printf("##### Get argument is %s\n", *argv);
+    printf("##### Get argc is %d\n"    , argc);
+    printf("##### Get argv is ");
     for(i=0;i<argc;i++)
     {
       printf(" %s" , argv[i]);
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 
     for(i=0;i<argc;i++)
     {
-      printf("#####Get argv[%d] is %s\n" ,i ,argv[i]);
+      printf("##### Get argv[%d] is %s\n" ,i ,argv[i]);
     }
   }
   #endif
@@ -53,12 +53,12 @@ int main(int argc, char *argv[])
     {
       if ((strcmp (argv[i], "--port") == 0))
       {
-        printf("#####get --port info: %s\n", argv[i+1]);
+        printf("##### get --port info: %s\n", argv[i+1]);
       }
 
       if ((strcasecmp (argv[i], "--file") == 0))
       {
-        printf("#####get --file info: %s\n", argv[i+1]);
+        printf("##### get --file info: %s\n", argv[i+1]);
       }
 
       i=i+2;
@@ -71,33 +71,53 @@ int main(int argc, char *argv[])
   {
     //ToCheck
     #define MAX_STRING_LENGTH 100
-    char input_arg_list[][2][MAX_STRING_LENGTH]=
+    char input_arg_list[][3][MAX_STRING_LENGTH]=
     {
-       {"--port", ""}
-      ,{"--file", ""}
-      ,{"--end--", "--end--"}
+       {"--port"  ,""         ,"set port name info"}
+      ,{"--file"  ,""         ,"set file name"}
+      ,{"--end--" ,"--end--"  ,"reserved keywords"}
     };
     printf("------------------------------\n");
 
-    for(i=1;i<argc;)
+    if ((strcasecmp (argv[1], "--help") == 0))
     {
-      //printf("##### Check argv[%d] info: %s\n" ,i ,argv[i]);
+      printf("##### Help arguments format:\n");
+      printf("##### --keyword key_value_need_input\n");
+      printf("##### keyword supported lists:\n");
       for (j=0;;j++)
       {
-        //printf("##### Check input_arg_list[%d][0] info: %s\n" ,i ,input_arg_list[j][0]);
         if ((strcasecmp (input_arg_list[j][0], "--end--") == 0)){break;}
-
-        //if (!strcmp (argv[i], "--file"))
-        if ((strcasecmp (argv[i], input_arg_list[j][0]) == 0))
-        {
-          //printf("#####get input_arg_list[j][0] info: %s\n", argv[i+1]);
-          //strcpy(input_arg_list[j][1], argv[i+1]);
-          //strncpy(input_arg_list[j][1], argv[i+1] ,MAX_STRING_LENGTH);
-          strncpy(input_arg_list[j][1], argv[i+1] ,strlen(argv[i+1]));
-          printf("#####Get %s info: %s\n" ,input_arg_list[j][0] ,input_arg_list[j][1]);
-        }
+        printf("#####     %s : %s\n" ,input_arg_list[j][0] ,input_arg_list[j][2]);
       }
-      i=i+2;
+
+      //return 1
+      value_returned =1;
+    }
+    else
+    {
+      for(i=1;i<argc;)
+      {
+        //printf("##### Check argv[%d] info: %s\n" ,i ,argv[i]);
+        for (j=0;;j++)
+        {
+          //printf("##### Check input_arg_list[%d][0] info: %s\n" ,i ,input_arg_list[j][0]);
+          if ((strcasecmp (input_arg_list[j][0], "--end--") == 0)){break;}
+
+          //if (!strcmp (argv[i], "--file"))
+          if ((strcasecmp (argv[i], input_arg_list[j][0]) == 0))
+          {
+            //printf("##### get input_arg_list[j][0] info: %s\n", argv[i+1]);
+            //strcpy(input_arg_list[j][1], argv[i+1]);
+            //strncpy(input_arg_list[j][1], argv[i+1] ,MAX_STRING_LENGTH);
+            strncpy(input_arg_list[j][1], argv[i+1] ,strlen(argv[i+1]));
+            printf("##### Get %s info: %s\n" ,input_arg_list[j][0] ,input_arg_list[j][1]);
+          }
+        }
+        i=i+2;
+      }
+
+      //return 0
+      value_returned =0;
     }
 
   }
