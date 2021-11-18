dc11097efe5364420b1f5a6f5eb91f0b288206a3
Frank Shan <frankqs0304@hotmail.com>
2021-11-18 12:58:31 +0800

    add time_stamp_log function in main, worked, backup before modulize it

--- 15_test_uart/test/src/test_uart.c
+++ 15_test_uart/test/src/test_uart.c
@@ -7,6 +7,70 @@
 //  extern char *_cl_port;
 //unsigned char * _write_data;
 
+#include <time.h>       /* time - run programs and summarize system resource usage */
+//struct timespec start_time[], last_stat[], last_timeout[], last_read[], last_write[];
+struct timespec current;
+struct timespec start_time, last_stat, last_timeout, last_read, last_write;
+//TODO//clock_gettime(CLOCK_MONOTONIC, &current);
+int init_time_stamp_log()
+{
+  //struct timespec current;
+  //struct timespec start_time, last_stat, last_timeout, last_read, last_write;
+  clock_gettime(CLOCK_MONOTONIC, &current);
+  start_time    = current;
+  last_stat     = current;
+  last_timeout  = current;
+  last_read     = current;
+  last_write    = current;
+  return 0;
+}
+
+
+int diff_ms(const struct timespec *t1, const struct timespec *t2)
+{
+  //ToCheck
+  struct timespec diff;
+  ///* POSIX.1b structure for a time value.  This is like a `struct timeval' but
+  //   has nanoseconds instead of microseconds.  */
+  //struct timespec
+  //  {
+  //    __time_t tv_sec;		/* Seconds.  */
+  //    __syscall_slong_t tv_nsec;	/* Nanoseconds.  */
+  //  };
+
+  //clock_gettime(CLOCK_MONOTONIC, &current);
+
+  diff.tv_sec = t1->tv_sec - t2->tv_sec;
+  diff.tv_nsec = t1->tv_nsec - t2->tv_nsec;
+  if (diff.tv_nsec < 0) {
+    diff.tv_sec--;
+    diff.tv_nsec += 1000000000;
+  }
+  //the unit is second
+  return (diff.tv_sec * 1000 + diff.tv_nsec/1000000);
+}
+
+int check_time_stamp_log_data()
+{
+  //struct timespec current;
+  //struct timespec start_time, last_stat, last_timeout, last_read, last_write;
+  clock_gettime(CLOCK_MONOTONIC, &current);
+  //start_time    = current;
+  //last_stat     = current;
+  //last_timeout  = current;
+  //last_read     = current;
+  //last_write    = current;
+  printf("start_time   diff is  %.1fs.\n", (double)diff_ms(&current, &start_time   ) / 1000);
+  printf("last_stat    diff is  %.1fs.\n", (double)diff_ms(&current, &last_stat    ) / 1000);
+  printf("last_timeout diff is  %.1fs.\n", (double)diff_ms(&current, &last_timeout ) / 1000);
+  printf("last_read    diff is  %.1fs.\n", (double)diff_ms(&current, &last_read    ) / 1000);
+  printf("last_write   diff is  %.1fs.\n", (double)diff_ms(&current, &last_write   ) / 1000);
+
+  return 0;
+}
+
+
+
 int main()
 {
   #if PRINT_DEBUG_ENABLE
@@ -41,11 +105,20 @@ int main()
     //clear the status of serial
     clear_custom_speed_flag();
 
+    #if 1
+    //for time_log check here
+    init_time_stamp_log();
+    #endif
     //try to write hello string
     write_hello_string();
 
     //try to read the data from serial port
     read_one_time_string();
+
+    #if 1
+    //for time_log check here
+    check_time_stamp_log_data();
+    #endif
 
     atexit(&exit_handler);
   #endif
